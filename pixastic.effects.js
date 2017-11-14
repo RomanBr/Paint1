53457112f5ceeb14136f11b8443a8e055b57e1b6
Roman Braverman <Ronenbr60@gmail.com>
2017-11-14 03:23:53 +0200

    Add files via upload

--- pixastic.effects.js
+++ pixastic.effects.js
@@ -1,40 +1,36 @@
 Pixastic.Effects = (function() {
 
     return {
+		morph : function(inData, outData, width, height, options, progress) {
+            var r=0, g=0, b=0, c, y, x, x1, y1, idx;
+			var F = Math.max;
+			for (y = 1; y < height - 1; y++) {
+				for (x = 1; x < width - 1; x++) {
+					idx = (x + y * width) * 4;
 
-        morph : function(inData, outData, width, height, options, progress) {
-            var r, g, b, c, y, x, x1, y1, idx,
-                pyc, pyp, pyn,
-                pxc, pxp, pxn,
-                minR, minG, minB, maxR, maxG, maxB,
-                n, prog, lastProg = 0;
-                
-            n = width * height * 4;
-                
-            for (y=0;y<height;y++) {
-                for (x=0;x<width;x++) {
-				r=0; b=0; g=0;
-				for (y1=y-1;y1<y+2;y1++) {
-					if (y1 < 0 || y1 > height) continue;
-					for (x1=x-1; x1 < x+2;x1++) {
-						if (x1 < 0 || x1 > width) continue;
-						
-						idx = (y1 * width + x1) * 4;
-						if (idx < 0 || idx > width*height*4) continue;
+					for (y1 = y - 1; y1 <= y + 1; y1++) {
+						if ((y1 < 0) || (y1 > height)) continue;
+						for (x1 = x - 1; x1 <= x + 1; x1++) {
+							if ((x1 < 0) || (x1 > width)) continue;
+							idx1 = (x1 + y1 * width) * 4;
+							if ((idx1 > outData.length) || (idx1 < 0 )) continue;
+							
+							r = F(r, inData[idx1 + 0]);
+							g = F(g, inData[idx1 + 1]);
+							b = F(b, inData[idx1 + 2]);
+						}
+					}
 
-						r = (r | inData[idx]);
-						g = (g | inData[idx + 1]);
-						b = (b | inData[idx + 2]);
-                    }
-				}
-                    
-                    outData[idx] = r;
-                    outData[idx+1] = g;
-                    outData[idx+2] = b;
-                    outData[idx+3] = 255;
+					outData[idx + 0] = r; // red
+					outData[idx + 1] = g; // green
+					outData[idx + 2] = b; // blue
+					outData[idx + 3] = 255; // alpha
 					
-                }
-            }
+					var r = 0, g = 0, b = 0;
+
+				}
+			}
 		} 
+
     }
 })();
